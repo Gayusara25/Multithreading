c7f9c9affa96b171ee9ed0d0eb9acb96f33cfebb
gayathri.ss <ssgayathri2001@gmail.com>
2023-09-13 16:44:55 +0530

    Add project files.

---
+++ Multithreading/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+namespace Multithreading
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Thread T1 = new Thread(Write);
+            Thread T2 = new Thread(Read);
+            T1.Start();
+            T2.Start();
+            Console.ReadKey();
+        }
+
+        static void Write()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"Writing is in process");
+
+                if (i == 5)
+                {
+                    Console.WriteLine("Writing is on hold");
+                    Thread.Sleep(5000);
+                    Console.WriteLine("Writing is wokeup");
+                }
+            }
+            
+        }
+        static void Read()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Reading is in process");
+            }
+
+        }
+    }
+}
